906d2a74a5e6bca9c8439c7d145a9c70af426122
TahriSamarIrfane <ls_tahri@esi.dz>
2025-01-07 09:44:37 +0100

    small cahnges

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,6 @@ dependencies {
 
     implementation("io.github.jan-tennert.supabase:auth-kt:3.0.3")
 
-
     implementation("androidx.credentials:credentials:1.3.0")
     implementation ("com.google.android.libraries.identity.googleid:googleid:1.1.1")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
